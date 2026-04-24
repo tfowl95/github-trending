a565a53e2e30243c4e13b93a3f56db4187520505
Thomas Fowler <tfowl95@gmail.com>
2026-04-23 23:53:04 -0500

    implemented request linking and returned the correct number of repos

--- utils.py
+++ utils.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+from datetime import datetime, timedelta
 import urllib.request
 import json
 
@@ -15,18 +15,39 @@ def create_url(duration, limit):
     per_page = min(100, limit)
     return f"https://api.github.com/search/repositories?q=created%3A%3E{start_date}&per_page={per_page}&sort=stars&order=desc"
 
-def get_repos(url):
-    req = urllib.request.Request(url)
-    req.add_header("Accept", "application/vnd.github+json")
-    try:
-        with urllib.request.urlopen(req) as response:
-            response_headers = dict(response.headers)
-            links = response_headers["Link"]
-            get_next_link = parse_links(links)
-            data = response.read()
-            return json.loads(data)
-    except Exception as e:
-        print(f"Error: {e}")
+def get_repos(url, limit):
+    repos_returned = 0
+    repos = []
+    while repos_returned < limit:
+        req = urllib.request.Request(url)
+        req.add_header("Accept", "application/vnd.github+json")
+        try:
+            with urllib.request.urlopen(req) as response:
+                response_headers = dict(response.headers)
+                links_header = response_headers["Link"]
+                url = get_next_url(links_header)
+                data = response.read()
+                payload = json.loads(data)
+                new_repos = payload["items"]
+                length = len(new_repos)
+                print(length)
+                repos_returned += length
+                if repos_returned > limit:
+                    remaining_num_repos = limit - len(repos)
+                    repos.extend(new_repos[0:remaining_num_repos])
+                else:
+                    repos.extend(new_repos)
+        except Exception as e:
+            print(f"Error: {e}")
+    return repos
 
-def parse_links(links):
-    pass
+def get_next_url(links_header):
+    links = {}
+    parts = links_header.strip().split(",")
+    for part in parts:
+        section = part.strip().split(";")
+        url = section[0].strip()[1:-1]
+        k, v = section[1].strip().split("=")
+        v = v.strip('"')
+        if v == "next":
+            return url
